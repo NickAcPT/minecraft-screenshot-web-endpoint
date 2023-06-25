e62100091d1f0bcff57fc2863f83e24726d94587
NickAc <32451103+NickAcPT@users.noreply.github.com>
2023-06-25 17:09:11 +0100

    Fix this

--- src/main/kotlin/io/github/nickacpt/minecraftscreenshotendpoint/MinecraftScreenshotEndpointMod.kt
+++ src/main/kotlin/io/github/nickacpt/minecraftscreenshotendpoint/MinecraftScreenshotEndpointMod.kt
@@ -1,15 +1,15 @@
 package io.github.nickacpt.minecraftscreenshotendpoint
 
 import io.github.nickacpt.minecraftscreenshotendpoint.controllers.screenshotRoute
-import net.fabricmc.api.ModInitializer
 import kotlin.concurrent.thread
 
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.jetty.*
+import net.fabricmc.api.ClientModInitializer
 
-object MinecraftScreenshotEndpointMod : ModInitializer {
-	override fun onInitialize() {
+object MinecraftScreenshotEndpointMod : ClientModInitializer {
+	override fun onInitializeClient() {
 		thread {
 			initializeKtor()
 		}
@@ -22,7 +22,7 @@ object MinecraftScreenshotEndpointMod : ModInitializer {
 
 	private fun initializeKtor() {
 		embeddedServer(Jetty, port = 36210, host = "127.0.0.1", module = Application::module)
-				.start(wait = true)
+			.start(wait = true)
 	}
 }
 
