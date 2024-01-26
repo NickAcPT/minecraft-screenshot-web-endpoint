e6210009e0190374ab33bc2d04bf3fc4b361c9b4
NickAc <32451103+NickAcPT@users.noreply.github.com>
2024-01-26 23:00:03 +0000

    Get frame skipping working

--- src/main/java/io/github/nickacpt/minecraftscreenshotendpoint/mixin/GameRendererMixin.java
+++ src/main/java/io/github/nickacpt/minecraftscreenshotendpoint/mixin/GameRendererMixin.java
@@ -4,7 +4,6 @@ import io.github.nickacpt.minecraftscreenshotendpoint.ScreenshotTaskHolder;
 import io.github.nickacpt.minecraftscreenshotendpoint.queue.ScreenshotQueue;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.GameRenderer;
-import net.minecraft.client.util.Window;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -30,8 +29,8 @@ public abstract class GameRendererMixin {
         }
     }
 
-    @Inject(method = "isRenderingPanorama", at = @At("HEAD"), cancellable = true)
-    private void mse$overrideRenderingPanorama(CallbackInfoReturnable<Boolean> cir) {
+    @Inject(method = "shouldRenderBlockOutline", at = @At("HEAD"), cancellable = true)
+    private void mse$overrideshouldRenderBlockOutline(CallbackInfoReturnable<Boolean> cir) {
         var holder = (ScreenshotTaskHolder) MinecraftClient.getInstance();
         var task = holder.mse$getCurrentScreenshotEntryTask();
 
--- src/main/kotlin/io/github/nickacpt/minecraftscreenshotendpoint/queue/ScreenshotQueue.kt
+++ src/main/kotlin/io/github/nickacpt/minecraftscreenshotendpoint/queue/ScreenshotQueue.kt
@@ -1,17 +1,27 @@
 package io.github.nickacpt.minecraftscreenshotendpoint.queue
 
+import io.github.nickacpt.minecraftscreenshotendpoint.mixin.MinecraftClientMixin
+import net.minecraft.client.MinecraftClient
 import java.util.concurrent.ConcurrentLinkedDeque
 
 object ScreenshotQueue {
     val size: Int get() = queue.size
-
     private val queue = ConcurrentLinkedDeque<ScreenshotQueueEntry>()
+
+    var skipNextFrameFlip = false
 
     fun addEntry(entry: ScreenshotQueueEntry) {
         queue.add(entry)
     }
 
     fun getNextEntry(): ScreenshotQueueEntry? {
-        return queue.poll()
+        return queue.poll().also { it: ScreenshotQueueEntry? ->
+            val isTakingScreenshot = it != null
+
+            skipNextFrameFlip = isTakingScreenshot
+            MinecraftClient.getInstance().gameRenderer?.apply {
+                isRenderingPanorama = isTakingScreenshot
+            }
+        }
     }
 }
