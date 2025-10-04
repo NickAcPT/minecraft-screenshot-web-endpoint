725e0fa756bfcca13a44d9adc93f76551861c9a6
NickAc <32451103+NickAcPT@users.noreply.github.com>
2025-10-04 15:25:47 +0100

    Add CORS, move to Jetty Jakarta and bump coroutines core jvm

--- src/main/kotlin/io/github/nickacpt/minecraftscreenshotendpoint/MinecraftScreenshotEndpointMod.kt
+++ src/main/kotlin/io/github/nickacpt/minecraftscreenshotendpoint/MinecraftScreenshotEndpointMod.kt
@@ -2,32 +2,43 @@ package io.github.nickacpt.minecraftscreenshotendpoint
 
 import io.github.nickacpt.minecraftscreenshotendpoint.controllers.screenshotRoute
 import io.github.nickacpt.minecraftscreenshotendpoint.controllers.teleportRoute
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
-import io.ktor.server.jetty.*
+import io.ktor.server.jetty.jakarta.Jetty
+import io.ktor.server.plugins.cors.routing.CORS
 import net.fabricmc.api.ClientModInitializer
-import net.minecraft.client.MinecraftClient
 import kotlin.concurrent.thread
 
 object MinecraftScreenshotEndpointMod : ClientModInitializer {
-	override fun onInitializeClient() {
-		thread {
-			initializeKtor()
-		}
-	}
+    override fun onInitializeClient() {
+        thread {
+            initializeKtor()
+        }
+    }
 
-	@JvmStatic
-	fun main(args: Array<String>) {
-		initializeKtor()
-	}
+    @JvmStatic
+    fun main(args: Array<String>) {
+        initializeKtor()
+    }
 
-	private fun initializeKtor() {
-		embeddedServer(Jetty, port = 36210, host = "127.0.0.1", module = Application::module)
-			.start(wait = true)
-	}
+    private fun initializeKtor() {
+        embeddedServer(Jetty, port = 36210, host = "127.0.0.1", module = Application::module)
+            .start(wait = true)
+    }
 }
 
 fun Application.module() {
-	screenshotRoute()
-	teleportRoute()
+    install(CORS) {
+        allowMethod(HttpMethod.Options)
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Delete)
+        allowMethod(HttpMethod.Patch)
+        allowHeader(HttpHeaders.Authorization)
+        anyHost()
+    }
+
+    screenshotRoute()
+    teleportRoute()
 }
